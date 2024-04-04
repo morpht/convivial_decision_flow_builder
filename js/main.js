498b9027780716fe03dddd65fde0fc452514b0fc
Abhisek Mazumdar <abhisekmazumdar@users.noreply.github.com>
2024-04-04 20:08:18 +0530

    dtb-36610: Add drop-down button for loading example from file. (#1)

--- js/main.js
+++ js/main.js
@@ -35,6 +35,19 @@ document.addEventListener('DOMContentLoaded', function() {
           });
     });
 
+    document.querySelectorAll('.dropdown-item.example').forEach(span => {
+        span.addEventListener('click', function() {
+            switch (this.id) {
+                case "simple-example":
+                    fetchExamplesFromFile("simple-example", './examples/index.html');
+                    break;
+                case "bookshop-example":
+                    fetchExamplesFromFile("bookshop-example", './examples/bookshop.html');
+                    break;
+            }
+        });
+    });
+
 });
 
 /**
@@ -43,4 +56,22 @@ document.addEventListener('DOMContentLoaded', function() {
 function clearExistingStorage() {
     localStorage.clear();
     document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
+}
+
+/**
+ * Function to fetch HTML data from example files and update the UI elements
+ *
+ * @param {string} id
+ * @param {string} filePath
+ */
+function fetchExamplesFromFile(id, filePath) {
+    fetch(filePath)
+        .then(response => response.text())
+        .then(html => {
+            htmlInput.value = html;
+            treeOutput.innerHTML = html;
+            refreshBtn.click();
+            refreshBtn.classList.remove('disabled');
+        })
+        .catch(error => console.error('Error fetching HTML file:', error));
 }
